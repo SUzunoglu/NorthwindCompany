1078d0e29cab686488b9d573c13c44b656c84f1b
sinan.uzunoglu@outlook.com <sinan.uzunoglu@outlook.com>
2021-04-10 17:40:17 +0300

    Entityframework ile veritabanı bağlantısı kuruldu, ürünler listelendi

---
+++ Entities/Concrete/Customer.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Customer:IEntity
+    {
+        public string CustomerId { get; set; } //northwind veritabanında customerId string olarak tutuluyor
+        public string ContactName { get; set; }
+        public string CompanyName { get; set; }
+        public string City { get; set; }
+    }
+}
