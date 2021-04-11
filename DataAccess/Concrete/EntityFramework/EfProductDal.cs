adca85fe918f12da9f67238164a59cb7d6b79f58
sinan.uzunoglu@outlook.com <sinan.uzunoglu@outlook.com>
2021-04-11 22:31:05 +0300

    ilk API olusturuldu

--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -20,6 +20,8 @@ namespace DataAccess.Concrete.EntityFramework
                 var result = from p in context.Products
                              join c in context.Categories
                              on p.CategoryId equals c.CategoryId
+                             where p.UnitPrice>50
+                             orderby p.UnitPrice descending
                              select new ProductDetailDto 
                              { 
                                  ProductId = p.ProductId, ProductName = p.ProductName, 
