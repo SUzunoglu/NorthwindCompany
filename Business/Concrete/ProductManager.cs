947cb9dc27fa7a67ecb24a83210bcbc2aa278379
sinan.uzunoglu@outlook.com <sinan.uzunoglu@outlook.com>
2021-05-14 10:53:26 +0300

    Register ve login işlemleri gerçekleşti postman'de test edildi

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Business.BusinessAspects.Autofac;
 using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
@@ -28,6 +29,7 @@ namespace Business.Concrete
             _categoryService = categoryService;
         }
 
+        [SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
@@ -44,7 +46,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 11)
+            if (DateTime.Now.Hour == 9)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
